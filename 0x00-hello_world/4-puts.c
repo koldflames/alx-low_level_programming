efd16ebd7e0a499f992542ddb5c011e0aa5466a7
koldflames <enoeihoda@gmail.com>
2023-02-16 15:47:59 -0800

    c code using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /*Prints the string to the console using the puts function */
+puts("Programming is like buidling a multilingual puzzle\n");
+return (0);
+}
