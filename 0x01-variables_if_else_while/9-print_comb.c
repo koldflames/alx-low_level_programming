4b165cda7633b70bc7b4154ab7e373bafc840ad0
koldflames <enoeihoda@gmail.com>
2023-02-21 21:52:45 -0800

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,36 +9,16 @@
  */
 int main(void)
 {
-int i, j, k, l;
-for (i = 0; i < 10; i++)
+int i;
+for (i = 48; i < 58; i++)
 {
-putchar('0' + i);
-putchar(',');
-putchar(' ');
-for (j = i + 1; j < 10; j++)
+putchar(i);
+if (i != 57)
 {
-putchar('0' + i);
-putchar('0' + j);
-putchar(',');
-putchar(' ');
-for (k = j + 1; k < 10; k++)
-{
-putchar('0' + i);
-putchar('0' + j);
-putchar('0' + k);
-putchar(',');
-putchar(' ');
-for (l = k + 1; l < 10; l++)
-{
-putchar('0' + i);
-putchar('0' + j);
-putchar('0' + k);
-putchar('0' + l);
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
+putchar('\n');
 return (0);
 }
