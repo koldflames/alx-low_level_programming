ebc23d809a1e8f2b87c4ba2d846937bfb60a047d
koldflames <enoeihoda@gmail.com>
2023-02-21 21:16:40 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - the entry point of the program
+ *Description - prints out sizes of each type
+ *Return: 0
+ *Description: stops the program
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+printf("\n");
+return (0);
+}
