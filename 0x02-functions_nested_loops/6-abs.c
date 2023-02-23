117a617c6584ebac4ba896965cc202d31ae12f5f
koldflames <enoeihoda@gmail.com>
2023-02-22 19:09:17 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - the entry point of the program
+ *Description - prints the lowercase alphabets and a new line
+ *Return: 0
+ *Description: stops the program
+ *@n: computes absolute value of an interger
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
