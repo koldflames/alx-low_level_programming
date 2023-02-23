2878131f3ebc8c9ef4c19180c07bfd86d8fda226
koldflames <enoeihoda@gmail.com>
2023-02-22 19:22:51 -0800

    24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *jack_bauer - prints every minute
+ *Description - prints the sign of a number
+ *Return: 0
+ *Description: stops the program
+ */
+void jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
+}
