8e1b3625771ded3180e89cb84aeb884ed10b98ad
koldflames <enoeihoda@gmail.com>
2023-02-22 18:11:16 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - the entry point of the program
+ *Description - prints the lowercase alphabets and a new line
+ *Return: 0
+ *Description: stops the program
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return 1;
+}
+else
+{
+return (0); 
+}
+}
