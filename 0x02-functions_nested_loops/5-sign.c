33a1c3d923ebc7bb6c6408ff2f173f4f6ca0c9c3
koldflames <enoeihoda@gmail.com>
2023-02-22 18:46:06 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - the entry point of the program
+ *Description - prints the sign of a number
+ *Return: 0
+ *Description: stops the program
+ *@n: function that checks for lower case character
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1);
+}
+else if (n < 0)
+{
+printf("-");
+return (-1);
+}
+else
+{
+printf("0");
+return (0);
+}
+}
