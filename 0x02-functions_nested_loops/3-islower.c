c58690292f5b57401e9f285a04d655c28a4a66c0
koldflames <enoeihoda@gmail.com>
2023-02-22 18:15:24 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_alphabet - the entry point of the program
+ *_islower - the entry point of the program
  *Description - prints the lowercase alphabets and a new line
  *Return: 0
  *Description: stops the program
