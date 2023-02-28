6e458ce05c28b06eb5ec303bdd3022e1e13ad9ae
Koldflames <enoeihoda@gmail.com>
2023-02-28 17:04:57 +0100

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description - Really? Combination again! Three!!!
- * Return: 0
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Return: All good
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 99; i++)
-{
-for (j = i; j <= 99; j++)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
-if (i < 99 || j < 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-return (0);
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
 }
