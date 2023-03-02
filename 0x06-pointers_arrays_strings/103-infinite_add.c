77437f38ef58c4e976759a85c550c18b17edb88e
Koldflames <enoeihoda@gmail.com>
2023-03-02 13:10:19 +0100

    infinite add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * infinite_add - Add two numbers
+ * Return: Always 0.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	
+
+}
+
+int looper1(char *n1, char *n2, int indexer, int number, int tens)
+{
+	n
+}
+
+int looper2(char *n1, char *n2, int indexer, int number, int tens)
+{}
+
+int looper3(char *n1, char *n2, int indexer int number, int tens)
+{}
+
+char *adding_str(char *n1, char *n2, char *r, int r_index)
+{}
