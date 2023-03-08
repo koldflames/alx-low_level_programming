905d6a42c73b2b093c24d478fa49ec05b1a3b0cd
Koldflames <enoeihoda@gmail.com>
2023-03-08 22:25:45 +0100

    Wild comp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,29 +2,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
 	if (*s2 == '*')
 	{
-		/* skip multiple * characters in s2 */
-		while (*(s2 + 1) == '*')
-			s2++;
-
-		/* try all possibilities of matching * in s1 */
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
-
-		/* if none of the possibilities work, return 0 */
-		return (wildcmp(s1, s2 + 1));
+		while (*++s2 == '*')
+			;
+		if (*s2 == '\0')
+			return (1);
+		while (*s1 != '\0' && !wildcmp(s1++, s2))
+			;
+		return (*s1 != '\0');
 	}
 
 	if (*s1 != '\0' && (*s1 == *s2 || *s2 == '?'))
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(++s1, ++s2));
 
 	return (0);
 }
