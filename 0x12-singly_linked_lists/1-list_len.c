aa9b7b608aabbdd01ceb9f5f7d54d03540a980ef
koldflames <enoeihoda@gmail.com>
2023-03-30 23:41:38 +0100

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,14 +5,15 @@
  * Return: number of elements in the list.
  */
 
-size_t list_len(const list_t *h) {
-    size_t count = 0;
-    const node_t *current = h->head; /* start at the head of the list */
+size_t list_len(const list_t *h)
+{
+	size_t number_of_nodes;
 
-    while (current != NULL) { /* traverse the list until the end */
-        count++; /* increment the counter */
-        current = current->next; /*move to the next node*/
-    }
-
-    return count; /*return the final count*/
+	number_of_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
 }
