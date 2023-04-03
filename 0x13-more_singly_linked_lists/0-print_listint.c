37cf5dd00e0eec63a490e0217e97015506a73b23
koldflames <enoeihoda@gmail.com>
2023-04-03 22:15:27 +0100

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,8 @@ size_t print_listint(const listint_t *h)
 {
 	/* Introduce a counter to keep track of the number of nodes passed through */
 	size_t count = 0;
-
-	/*
-	 * Use a while loop to do an iteration over the nodes of the list,
-	 * and also to print the value given to the 'n' member of the current node in check 
-	 */
-       while (h != NULL)
+	
+	while (h != NULL)
        {
 	       printf("%d\n", h->n);
 	       h = h->next;
