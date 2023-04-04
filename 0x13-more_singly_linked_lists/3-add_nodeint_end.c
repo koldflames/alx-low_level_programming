ad8001a0c32c10baf4c77ee83c381a2ec4fee17e
koldflames <enoeihoda@gmail.com>
2023-04-04 14:25:26 -0700

    Updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,7 @@
  *
  * Return: the address of the newly added element, or NULL if it failed.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n);
 {
 	listint_t *new_node, *current_node;
 	if (head == NULL)
