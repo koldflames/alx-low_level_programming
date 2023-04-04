fa4ccd4db1ce7e44defd32eb93d60df995ca7905
koldflames <enoeihoda@gmail.com>
2023-04-03 17:54:18 -0700

    Betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,6 +3,7 @@
 /**
  * print_listint - prints all the elements of the listint_t list
  * @h: the pointer of head of the list
+ *
  * Return: the number of nodes present in the list
  */
 size_t print_listint(const listint_t *h)
