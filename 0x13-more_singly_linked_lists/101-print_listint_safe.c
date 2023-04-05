7aebd4c281118dd0e6c3876fae26971f1ab11217
koldflames <enoeihoda@gmail.com>
2023-04-04 18:50:08 -0700

    Correction on task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,61 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - counts the number of nodes in a circular list
- * @head: pointer pointing to the first node
- * Return: returns the number of nodes and 0 otherwise
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t len = 0, i = 0;
-	const listint_t *temp = head;
+	size_t nnodes = 0;
+	listp_t *hptr = NULL, *new = NULL, *add = NULL;
 
-	if (head == NULL)
-		return (0);
-
-	while (temp && temp > temp->next)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		temp = temp->next;
-		len++;
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
-
-	if (temp)
-	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
-		len++;
-	}
-
-	i = len;
-
-	temp = head;
-	while (i--)
-		printf("[%p] %d\n", (void *)temp++, temp->n);
-
-	return (len);
+	free_listp(&hptr);
+	return (nnodes);
 }
