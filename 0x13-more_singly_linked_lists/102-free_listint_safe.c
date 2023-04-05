368d2d3b5d6ec3482205a66848ca57356ffe20f8
koldflames <enoeihoda@gmail.com>
2023-04-04 19:24:06 -0700

    solution to task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - this function frees a linked list
+ * @head: pointer to head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return;
+
+	free_listp(&((*head)->next));
+	free(*head);
+	*head = NULL;
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *visited = NULL;
+	listint_t *curr = *h;
+
+	while (curr != NULL)
+	{
+		listp_t *node = malloc(sizeof(listp_t));
+
+		if (node == NULL)
+		{
+			free_listp(&visited);
+			exit(98);
+		}
+		node->p = (void *)curr;
+		node->next = visited;
+		visited = node;
+
+		listp_t *p = visited->next;
+
+		while (p != NULL)
+		{
+			if (p->p == visited->p)
+			{
+				*h = NULL
+				free_listp(&visited);
+				return (nnodes);
+			}
+			p = p->next;
+		}
+
+		listint_t *next = curr->next;
+
+		free(curr);
+		curr = next;
+		nnodes++;
+	}
+
+	*h = NULL;
+	free_listp(&visited);
+	return (nnodes);
+}
