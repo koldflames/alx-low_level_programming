d09f25a15fcb640f31f28f9c616be573b928403f
koldflames <enoeihoda@gmail.com>
2023-04-04 18:01:16 -0700

    Solution to task 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+
+		if (current >= current->next)
+		{
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			exit(98);
+		}
+
+		current = current->next;
+	}
+			return (count);
+}
