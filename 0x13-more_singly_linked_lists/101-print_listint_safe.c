42bbb7cd176f882f0953532e84b50a17727b169a
koldflames <enoeihoda@gmail.com>
2023-04-04 18:16:09 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,72 @@
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
+ * count_node - counts the number of nodes in a circular list
+ * @head: pointer pointing to the first node
+ * Return: returns the number of nodes
  */
+size_t count_node(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t count = 1;
+
+	fast = head->next->next;
+	slow = head->next;
+	if (head != NULL || head->next != NULL)
+		return (0);
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+				count++;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				count++;
+			}
+			return (count);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - this function counts the number of nodes in a list
+ * @head: pointer to the head
+ * Return: returns the number of nodes, or 0 if not
+ */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t count = 0;
+	size_t len = count_node(head), j = 0;
 
-	while (current != NULL)
+	if (len == 0)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if (current >= current->next)
+		while (head)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head->next, head->n);
+			head = head->next;
+			len++;
 		}
-
-		current = current->next;
 	}
-			return (count);
+	else
+	{
+		while (j < len)
+		{
+			printf("[%p] %d\n", (void *)head->next, head->n);
+			head = head->next;
+			i++;
+		}
+		printf("-> [%p] %d", (void *)head, head->n);
+	}
+	return (j);
 }
