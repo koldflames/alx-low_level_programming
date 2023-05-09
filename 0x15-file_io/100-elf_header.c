1de2b0d371c2cedb7525e21dcf0772abf33198d5
koldflames <enoeihoda@gmail.com>
2023-05-09 04:18:49 -0700

    update to task 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,119 +1,44 @@
 #include "main.h"
 
 /**
- * if_elf - this function checks if the file is an ELF file.
- * @magicptr: this points to an array that contains the ELF magic numbers
- *
- * Description: the exit cod 98 is used if the file is not ELF
+ * main - reads the header of an ELF and checks whether it is a valid ELF file or not
+ * @argc: this is the argument that is checked
+ * @argv: this is the argument that is passed
  */
-
-void if_elf(unsigned char *magicptr)
+int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 0; i < 4; i++)
+	int fd;
+	char elf_header[16];
+	ssize_t nread;
+
+	if (argc != 2)
 	{
-		if (magicptr[i] != 127 && magicptr[i] != 'E' && magicptr[i] != 'L' && magicptr[i] != 'F')
-		{
-			fprintf(stderr, "Error: Not an ELF file\n");
-			exit(98);
-		}
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
-}
+	fd = open(argv[1], O_RDONLY);
 
-/**
- * disp_magic - this function displays the ELF header magic numbers
- * @magicptr: this points to an array that contains the ELF magic numbers
- *
- * Description: the Magic numbers are displayed and seperated by spaces
- */
-
-void disp_magic(unsigned char *magicptr)
-{
-	int i;
-	printf("  Magic:  ");
-	for (i = 0; i < EINIDENT; i++)
+	if (fd == -1)
 	{
-		printf("%02x", magicptr[i]);
-
-		if (i == EI_NIDENT - 1)
-		{
-			printf("\n");
-		} else
-		{
-			printf(" ");
-		}
+		perror("open");
+		exit(EXIT_FAILURE);
 	}
-}
+	nread = read(fd, elf_header, 16);
 
-/**
- * disp_class - this function displays the ELF header's class
- * @magicptr: this points to an array that contains the ELF magic numbers.
- *
- * Description: class is printed
- */
-
-void disp_class(unsigned char *magicptr)
-{
-	printf("  Class:                             ");
-	switch (magicptr[EI_CLASS])
+	if (nread != 16)
 	{
-		case ELFCLASSNONE:
-			printf("none\n");
-			break;
-		case ELFCLASS32:
-			printf("ELF32\n");
-			break;
-		case ELFCLASS64:
-			printf("ELF64\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", magic[EI_CLASS]);
+		fprintf(stderr, "Error: Not an ELF file - it has the wrong magic bytes at the start\n");
+		close(fd);
+		exit(EXIT_FAILURE);
 	}
-}
 
-/**
- * disp_data - this function displays the ELF header's data
- * @magicptr: this points to an array that contains the ELF magic numbers.
- *
- * Description: data is printed
- */
-
-void disp_data(unsigned char *magicptr)
-{
-	printf("  Data:                              ");
-	switch (magicptr[EI_DATA])
+	if (memcmp(elf_header, ELF_MAGIC_BYTES, 4) != 0)
 	{
-		case ELFDATANONE:
-			printf("none\n");
-			break;
-		case ELFDATA2LSB:
-			printf("2's complement, little endian\n");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big endian\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", magic[EI_CLASS]);
+		fprintf(stderr, "Error: Not an ELF file - it has the wrong magic bytes at the start\n");
+		close(fd);
+		exit(EXIT_FAILURE);
 	}
-}
-
-/**
- * disp_version - this function displays the ELF header's version
- * @magicptr: this points to an array that contains the ELF magic numbers.
- *
- * Description: prints the version
- */
-
-void disp_version(unsigned char *magic)
-{
-	printf("  Version:                           %d", magicptr[EI_VERSION]);
-	switch (magicptr[EI_VERSION])
-	{
-		case EV_CURRENT:
-			printf(" (current)\n");
-			break;
-		default:
-			printf("\n");
-			break;
-	}
+	printf("ELF file detected!\n");
+	close(fd);
+	exit(EXIT_SUCCESS);
 }
