ac866f3938aa42c74c5af25fc6eeea8befa40092
koldflames <enoeihoda@gmail.com>
2023-10-12 13:29:24 -0700

    task 1 updated

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
-/*
- * dlistint_len - Return list len
- * @h: struct list
- * Return: list_len
+/**
+ *dlistint_len - Return list length of a doubly-linked list
+ *Description -  This function counts the number of nodes in a doubly-list
+ *by traversing it from the head node to the end
+ *@h: Pointer to the head of the list
+ *
+ *Return: list_len
  */
 
 size_t dlistint_len(const dlistint_t *h)
